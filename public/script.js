7e3c4a977da63d7203138828322fb185258c210c
Armin Eslami <armin.eslami@gmail.com>
2017-10-26 00:54:17 -0400

    style: correct indentation

--- public/script.js
+++ public/script.js
@@ -26,39 +26,39 @@ Drink.prototype = {
 };
 
 var data = [
-			new Drink(
-				'1RdpoppRkOA',
-				{name: 'Coffee', price: 2},
-				[
-					{name: 'Milk', price: 1, count: 0},
-					{name: 'Sugar', price: 1, count: 0}
-				]
-			),
-			new Drink(
-				'RFHFV7lVQBY',
-				{name: 'Latte', price: 4},
-				[
-					{name: 'Chocolate', price: 2, count: 0},
-					{name: 'Cream', price: 2, count: 0}
-				]
-			),
-			new Drink(
-				'7liDpl93wt4',
-				{name: 'Tea', price: 2},
-				[
-					{name: 'Milk', price: 1, count: 0},
-					{name: 'Sugar', price: 1, count: 0}
-				]
-			),
-			new Drink(
-				'm741tj4Cz7M',
-				{name: 'Smoothie', price: 5},
-				[
-					{name: 'Protein', price: 2, count: 0},
-					{name: 'Sugar', price: 1, count: 0}
-				]
-			)
-		];
+	new Drink(
+		'1RdpoppRkOA',
+		{name: 'Coffee', price: 2},
+		[
+			{name: 'Milk', price: 1, count: 0},
+			{name: 'Sugar', price: 1, count: 0}
+		]
+	),
+	new Drink(
+		'RFHFV7lVQBY',
+		{name: 'Latte', price: 4},
+		[
+			{name: 'Chocolate', price: 2, count: 0},
+			{name: 'Cream', price: 2, count: 0}
+		]
+	),
+	new Drink(
+		'7liDpl93wt4',
+		{name: 'Tea', price: 2},
+		[
+			{name: 'Milk', price: 1, count: 0},
+			{name: 'Sugar', price: 1, count: 0}
+		]
+	),
+	new Drink(
+		'm741tj4Cz7M',
+		{name: 'Smoothie', price: 5},
+		[
+			{name: 'Protein', price: 2, count: 0},
+			{name: 'Sugar', price: 1, count: 0}
+		]
+	)
+];
 
 var app = new Vue({
 	el: '#app',
